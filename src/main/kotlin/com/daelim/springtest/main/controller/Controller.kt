2d175caec79997d5ed5de2b33f6d0da73ee3a5d5
KIBO0630 <155514331+KIBO0630@users.noreply.github.com>
2024-04-24 14:32:15 +0900

    랜덤 숫자 생성 코드 작성

--- src/main/kotlin/com/daelim/springtest/main/controller/Controller.kt
+++ src/main/kotlin/com/daelim/springtest/main/controller/Controller.kt
@@ -1,43 +1,22 @@
-package com.daelim.springtest.main.controller
-
-import com.daelim.springtest.main.api.model.dto.TestDto
-import com.daelim.springtest.main.api.model.dto.TestDtoRequest
-import io.swagger.v3.oas.annotations.Parameter
-import jakarta.validation.Valid
-import net.datafaker.Faker
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-
-@RestController
+import kotlin.random.Random
 class Controller {
     private val tests = mutableListOf<TestDto>()
 
-    @PostMapping("/test")
-    fun postTestDto(
-        @RequestBody testDtoRequest: TestDtoRequest
-    ): ResponseEntity<TestDto> {
-        val test = TestDto(
-            name = testDtoRequest.name,
-            job = testDtoRequest.job,
-            hp = Random().nextFloat(100F),
-            speed = Random().nextFloat(200F),
-            atk = Random().nextFloat(500F),
-        )
-        tests.add(test)
-        return ResponseEntity.ok().body(test)
-    }
-    @GetMapping("/test")
-    fun getAllTestDto(
-    ): ResponseEntity<List<TestDto>> {
-        val response = tests
-        return ResponseEntity.ok().body(response)
-    }
+    fun RandomLotto() {
+        val numberOfRows = 5
+        val numbersPerRow = 7
+        val minNumber = 1
+        val maxNumber = 45
 
-    @GetMapping("/test/{charName}")
-    fun getTestDto(
-        @PathVariable("charName") charName: String
-    ): ResponseEntity<TestDto> {
-        val response = tests.firstOrNull{it.name == charName}
-        return ResponseEntity.ok().body(response)
+        val rows = Array(numberOfRows) { IntArray(numbersPerRow) }
+
+        for (i in rows.indices) {
+            val generatedNumbers = mutableSetOf<Int>()
+            while (generatedNumbers.size < numbersPerRow) {
+                val randomNumber = Random.nextInt(minNumber, maxNumber + 1)
+                generatedNumbers.add(randomNumber)
+            }
+            rows[i] = generatedNumbers.toIntArray().sorted().toIntArray()
+        }
     }
 }
